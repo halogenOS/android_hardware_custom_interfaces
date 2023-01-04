0ef358c3ef8f444a64e883e20e567fded0a249a6
Simão Gomes Viana <devel@superboring.dev>
2023-01-04 18:41:44 +0100

    arrow -> custom

--- update-makefiles.sh
+++ update-makefiles.sh
@@ -3,6 +3,6 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.arrow:hardware/arrow/interfaces" \
+  "vendor.arrow:hardware/custom/interfaces" \
   "android.hardware:hardware/interfaces" \
   "android.hidl:system/libhidl/transport"
